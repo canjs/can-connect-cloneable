539458b299574c40de593bd160ffb1cf6f69c86b
Kyle Gifford <kyle@kylegifford.com>
2017-02-14 20:22:12 -0600

    initial DefineMap and CanMap implementation

--- can-connect-cloneable.js
+++ can-connect-cloneable.js
@@ -1,40 +1,52 @@
-var canMap = require('can-map');
-var each = require('can-util/js/each/each');
-var getIdProps = require('can-connect/helpers/get-id-props');
+// var each = require("can-util/js/each/each");
+var deepAssign = require("can-util/js/deep-assign/deep-assign");
+var getIdProps = require("can-connect/helpers/get-id-props");
 
 // makes a clone by wiring all of it's properties to read from ._original[prop]
 module.exports = function makeClone(Type) {
-	if (Type.prototype instanceof canMap) {
-		var idProp = getIdProps(Type.connection)[0];
-		var definition = {
-			define: {
-				_original: {
-					Type: Type,
-				},
-			},
-			save() {
-				var data = this.serialize();
-				delete data._original;
+	var idProp = getIdProps(Type.connection)[0];
+	var definition = {
+		save() {
+			var data = this.serialize();
+			delete data._original;
+
+			if (this._original[idProp]) {
 				data[idProp] = this._original[idProp];
-				this._original.serialize();
 				return this._original.constructor.connection.updateData(data).then(function() {
 					Type.connection.updatedInstance(this._original, data);
 				}.bind(this));
-			},
-		};
+			} else {
+				return this._original.constructor.connection.createData(data).then(function(res) {
+					Type.connection.createdInstance(this._original, deepAssign({}, data, res));
+				}.bind(this));
+			}
+		},
+	};
 
-		if (Type.prototype.hasOwnProperty('define')) {
-			each(Type.prototype.define, function(def, prop) {
-				definition.define[prop] = def;
-			});	
-		}
-		var Clone = Type.extend(definition);
-
-		Type.prototype.clone = function clone() {
-			var props = this.serialize();
-			delete props[idProp];
-			props._original = this;
-			return new Clone(props);
-		};
+	// Specific functions for DefineMap and CanMap
+	if (Type.prototype.hasOwnProperty('_define')) {
+		// add a reference to the original
+		deepAssign(definition, {
+			_original: {
+				Type: Type
+			}
+		});
+	} else {
+		// add a reference to the original
+		deepAssign(definition, {
+			define: {
+				_original: {
+					Type: Type
+				}
+			}
+		});
 	}
-}
+
+	var Clone = Type.extend(definition);
+	Type.prototype.clone = function clone() {
+		var props = this.serialize();
+		delete props[idProp];
+		props._original = this;
+		return new Clone(props);
+	};
+};
--- can-connect-cloneable_test.js
+++ can-connect-cloneable_test.js
@@ -1,34 +1,172 @@
-var QUnit = require('steal-qunit');
-var makeCloneable = require('./can-connect-cloneable');
-var CanMap = require('can-map');
-var CanList = require('can-list');
-require('can-map-define');
-var Fixture = require('can-fixture');
-var Connect = require('can-connect');
-var ConnectDataUrl = require('can-connect/data/url/url');
-var ConnectConstructor = require('can-connect/constructor/constructor');
-var ConnectConstructorCallbacksOnce = require('can-connect/constructor/callbacks-once/callbacks-once');
-var ConnectRealTime = require('can-connect/real-time/real-time');
-var ConnectCanMap = require('can-connect/can/map/map');
+var QUnit = require("steal-qunit");
+var makeCloneable = require("can-connect-cloneable");
+var DefineMap = require("can-define/map/map");
+require("can-define/list/list");
+var CanMap = require("can-map");
+require("can-map-define");
+require("can-list");
+var Fixture = require("can-fixture");
+var Connect = require("can-connect");
+var ConnectDataUrl = require("can-connect/data/url/url");
+var ConnectConstructor = require("can-connect/constructor/constructor");
+var ConnectCanMap = require("can-connect/can/map/map");
 
+Fixture("POST /test", { id: 1 });
 Fixture("PUT /test/1", {});
 
-QUnit.module('can-connect-cloneable');
+QUnit.module("can-connect-cloneable");
 
-	QUnit.test('Initialized the plugin', function() {
-	  	QUnit.equal(typeof makeCloneable, 'function');
+	QUnit.test("Initialized the plugin", function() {
+	  	QUnit.equal(typeof makeCloneable, "function");
 	});
 
-// implementations:
-// - DefineMap implementation
-// X without realtime
-// - with realtime + callbacks-once
+	QUnit.module("DefineMap", {
+		beforeEach: function(assert) {
+			this.runSetterAssertions = false;
+			var self = this;
+			this.CloneableDefineMap = DefineMap.extend({
+				info: {
+					get: function() {
+						return this.name + " is " + this.age;
+					}
+				},
+				age: {
+					value: 30,
+					type: "number",
+					set: function(age) {
+						if (self.runSetterAssertions) {
+							assert.ok(true, "setter ran");	
+						}
+						return age; 
+					}
+				},
+				name: {
+					value: ""
+				},
+				id: {
+					value: null
+				}
+			});
 
-	QUnit.module('DefineMap');
+			// create the connection
+			var connection = Connect([
+				ConnectDataUrl,
+				ConnectConstructor,
+				ConnectCanMap
+			],{
+				idProp: "id",
+				Map: this.CloneableDefineMap,
+				url: "/test",
+			});
 
-	QUnit.module('Cloning CanMap', {
-		beforeEach: function() {
-			this.CloneableMap = CanMap.extend({});
+			// manual wire up the canMap to the connection
+			this.CloneableDefineMap.connection = connection;
+
+			// apply the mixin
+			makeCloneable(this.CloneableDefineMap);
+		}
+	});
+
+		QUnit.test("Initialization", function() {
+			var noncloneableMap = new DefineMap({});
+			var cloneableMap = new (this.CloneableDefineMap)({});
+			var clone = cloneableMap.clone();
+
+			QUnit.ok(!noncloneableMap.constructor.prototype.hasOwnProperty("clone"), "base Map does not have a clone method");
+			QUnit.ok(cloneableMap.constructor.prototype.hasOwnProperty("clone"), "cloneableMap has a clone method");
+			QUnit.notEqual(cloneableMap, clone, "Clone is not the same instance");
+		});
+
+		QUnit.test("Saving an original without an ID does a create", function(assert) {
+			var done = assert.async();
+			var map = new (this.CloneableDefineMap)({
+				name: "Kyle"
+			});
+			var clone = map.clone();
+			clone.name = "Justin";
+
+			QUnit.equal(map.name, "Kyle", "original's name is still Kyle");
+			QUnit.equal(clone.name, "Justin", "clone's name is now Justin");
+
+			clone.save().then(function() {
+				QUnit.equal(map.name, "Justin", "original's name changed to Justin");
+				QUnit.equal(clone.name, "Justin", "clone's name is still Justin");
+				QUnit.ok(!map.hasOwnProperty("_original"), "original does not have the _original attribute");
+				QUnit.equal(map.id, 1, "original receives an ID");
+				QUnit.equal(clone.id, undefined, "clone still does not have an ID until saving");
+				done();
+			});
+		});
+
+		QUnit.test("Saving updates the original", function(assert) {
+			var done = assert.async();
+			var map = new (this.CloneableDefineMap)({
+				name: "Kyle",
+				id: 1
+			});
+			var clone = map.clone();
+			clone.name = "Justin";
+
+			QUnit.equal(map.name, "Kyle", "original's name is still Kyle");
+			QUnit.equal(clone.name, "Justin", "clone's name is now Justin");
+
+			clone.save().then(function() {
+				QUnit.equal(map.name, "Justin", "original's name changed to Justin");
+				QUnit.equal(clone.name, "Justin", "clone's name is still Justin");
+				QUnit.ok(!map.hasOwnProperty("_original"), "original does not have the _original attribute");
+				done();
+			});
+		});
+
+		QUnit.skip("Property changes from the original push to the clone", function() {
+			var map = new (this.CloneableDefineMap)({
+				name: "Kyle",
+				id: 1
+			});
+			var clone = map.clone();
+			QUnit.equal(clone.name, "Kyle", "clone's name has the value of original");
+			map.name = "Justin";
+			QUnit.equal(clone.name, "Justin", "clone's name changes when original's name changes");
+		});
+
+		QUnit.test("Getters/Setters/type transfers to the clone", 6, function(assert) {
+			this.runSetterAssertions = true;
+			var map = new (this.CloneableDefineMap)({
+				name: "Kyle",
+				id: 1
+			});
+			var clone = map.clone();
+
+			// setter runs once for map, twice for clone, and once here = 4x
+			clone.age = "32";
+			assert.ok(clone.age === 32, "type applied");
+			assert.equal(clone.info, "Kyle is 32", "getter applied");
+		});
+
+	QUnit.module("CanMap", {
+		beforeEach: function(assert) {
+			this.runSetterAssertions = false;
+			var self = this;
+			this.CloneableCanMap = CanMap.extend({
+				define: {
+					info: {
+						get: function() {
+							return this.attr("name") + " is " + this.attr("age");
+						}
+					},
+					age: {
+						value: 30,
+						type: "number",
+						set: function(age) {
+							if (self.runSetterAssertions) {
+								assert.ok(true, "setter ran");	
+							}
+							return age; 
+						}
+					}
+				},
+				name: "",
+			});
 
 			// create the connection
 			var connection = Connect([
@@ -36,36 +174,58 @@ QUnit.module('can-connect-cloneable');
 				ConnectConstructor,
 				ConnectCanMap,
 			],{
-				idProp: 'id',
-				Map: this.CloneableMap,
-				url: '/test',
+				idProp: "id",
+				Map: this.CloneableCanMap,
+				url: "/test",
 			});
 
 			// manual wire up the canMap to the connection
-			this.CloneableMap.connection = connection;
-
-			// QUnit.ok(!CloneableMap.hasOwnProperty("clone"), "cloneableMap does not have a clone property before applying the mixin")
+			this.CloneableCanMap.connection = connection;
 
 			// apply the mixin
-			makeCloneable(this.CloneableMap);
+			makeCloneable(this.CloneableCanMap);
 		}
 	});
 
-		QUnit.test('Cloning an original without an ID does ??', 0, function() {
+		QUnit.test("Initialization", function() {
+			var noncloneableMap = new CanMap({});
+			var cloneableMap = new (this.CloneableCanMap)({});
+			var clone = cloneableMap.clone();
 
+			QUnit.ok(!noncloneableMap.constructor.prototype.hasOwnProperty("clone"), "base Map does not have a clone method");
+			QUnit.ok(cloneableMap.constructor.prototype.hasOwnProperty("clone"), "cloneableMap has a clone method");
+			QUnit.notEqual(cloneableMap, clone, "Clone is not the same instance");
 		});
 
-		QUnit.test('Saving updates the original', function(assert) {
+		QUnit.test("Cloning an original without an ID does a create", function(assert) {
 			var done = assert.async();
-			var map = new (this.CloneableMap)({
-				name: 'Kyle',
+			var map = new (this.CloneableCanMap)({
+				name: "Kyle"
+			});
+			var clone = map.clone();
+			clone.attr("name", "Justin");
+
+			QUnit.equal(map.attr("name"), "Kyle", "original's name is still Kyle");
+			QUnit.equal(clone.attr("name"), "Justin", "clone's name is now Justin");
+
+			clone.save().then(function() {
+				QUnit.equal(map.attr("name"), "Justin", "original's name changed to Justin");
+				QUnit.equal(clone.attr("name"), "Justin", "clone's name is still Justin");
+				QUnit.ok(!map.hasOwnProperty("_original"), "original does not have the _original attribute");
+				QUnit.equal(map.attr("id"), 1, "original receives an ID");
+				QUnit.equal(clone.attr("id"), undefined, "clone still does not have an ID until saving");
+				done();
+			});
+		});
+
+		QUnit.test("Saving updates the original", function(assert) {
+			var done = assert.async();
+			var map = new (this.CloneableCanMap)({
+				name: "Kyle",
 				id: 1
 			});
 			var clone = map.clone();
-
-			QUnit.notEqual(map, clone, "Clone is not the same instance");
-
-			clone.attr('name', "Justin");
+			clone.attr("name", "Justin");
 
 			QUnit.equal(map.attr("name"), "Kyle", "original's name is still Kyle");
 			QUnit.equal(clone.attr("name"), "Justin", "clone's name is now Justin");
@@ -78,10 +238,27 @@ QUnit.module('can-connect-cloneable');
 			});
 		});
 
-		QUnit.test('Property changes from the original push to the clone', 0, function() {
-
+		QUnit.skip("Property changes from the original push to the clone", function() {
+			var map = new (this.CloneableCanMap)({
+				name: "Kyle",
+				id: 1
+			});
+			var clone = map.clone();
+			QUnit.equal(clone.attr("name"), "Kyle", "clone's name has the value of original");
+			map.attr("name", "Justin");
+			QUnit.equal(clone.attr("name"), "Justin", "clone's name changes when original's name changes");
 		});
 
-		QUnit.test('Define plugin configuration transfers to the clone', 0, function() {
+		QUnit.test("Define plugin configuration transfers to the clone", 5, function(assert) {
+			this.runSetterAssertions = true;
+			var map = new (this.CloneableCanMap)({
+				name: "Kyle",
+				id: 1
+			});
+			var clone = map.clone();
 
+			// setter runs once for map, once for clone, and once here = 3x
+			clone.attr("age", "32");
+			assert.ok(clone.attr("age") === 32, "type applied");
+			assert.equal(clone.attr("info"), "Kyle is 32", "getter applied");
 		});
